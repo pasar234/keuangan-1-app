999490beea8cdc97bda9fefe542dee9696048407
pasar234 <pasarpon21@gmail.com>
2026-02-01 21:18:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,17 @@
-function renderTables() {
-    const list = JSON.parse(localStorage.getItem('keuangan_data')) || [];
-    const hBody = document.querySelector('#tableHutang tbody');
-    const pBody = document.querySelector('#tablePiutang tbody');
-    
-    // Ambil tanggal hari ini (set ke jam 00:00 agar perbandingan akurat)
-    const today = new Date();
-    today.setHours(0, 0, 0, 0);
-
-    hBody.innerHTML = '';
-    pBody.innerHTML = '';
-
-    list.forEach(item => {
-        // Logika warna merah: Cek jika jatuh tempo <= hari ini
-        const tglJatuhTempo = new Date(item.jatuh_tempo);
-        const isOverdue = tglJatuhTempo <= today;
-        
-        // Jika overdue, tambahkan style warna merah dan tebal
-        const styleRed = isOverdue ? 'style="color: red; font-weight: bold;"' : '';
-
-        const row = `<tr>
-            <td>${item.tanggal}</td>
-            <td>${item.keterangan}</td>
-            <td>${item.jumlah}</td>
-            <td ${styleRed}>${item.jatuh_tempo || '-'}</td>
-            <td>${item.bayar}</td>
-        </tr>`;
-        
-        if(item.jenis === 'hutang') {
-            hBody.innerHTML += row;
-        } else {
-            pBody.innerHTML += row;
-        }
+function openScreen(screenId) {
+    // Sembunyikan semua screen
+    document.querySelectorAll('.screen').forEach(s => {
+        s.style.display = 'none';
     });
+    
+    // Tampilkan screen yang dipilih
+    const activeScreen = document.getElementById(screenId);
+    if (activeScreen) {
+        activeScreen.style.display = 'block';
+    }
+
+    // Refresh tabel jika membuka tab Data
+    if (screenId === 'data') {
+        renderTables();
+    }
 }
